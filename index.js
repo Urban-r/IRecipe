63d0c033f90c9a8784ee21f9a90135c3972df1cc
Riyaad Shahid <126833781+Urban-r@users.noreply.github.com>
2024-12-12 18:12:21 +0000

    Added security

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ global.db = db;
 
 
 // Define our application-specific data
-app.locals.webData = {webName: "IRecipe"}
+app.locals.webData = {webName: "iRecipe"}
 
 // Load the route handlers
 const mainRoutes = require("./routes/main")
